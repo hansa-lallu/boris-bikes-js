8598d006bac0d25702aa33ac3b3588af963d3307
hansa-lallu <hansalallu@gmail.com>
2020-06-23 06:32:05 +0100

    grouped tests

--- spec/dockingStationSpec.js
+++ spec/dockingStationSpec.js
@@ -13,33 +13,42 @@ describe('DockingStation', () => {
     expect(dockingStation.capacity).toEqual(30)
   });
 
-  it('can release a bike from a docking station', () => {
-    const dockingStation = new DockingStation();
-    dockingStation.dockBike()
-    expect(dockingStation.releaseBike()).toEqual('Here is your Bike');
+  describe('release bike', () => {
+    it('can release a bike from a docking station', () => {
+      const dockingStation = new DockingStation();
+      dockingStation.dockBike()
+      expect(dockingStation.releaseBike()).toEqual('Here is your Bike');
+    });
+  
+    it('does not release a bike if no bike is available', () => {
+      const dockingStation = new DockingStation();
+      expect(function () { dockingStation.releaseBike() }).toThrowError('No Bikes Available')
+    });
+  
+    // it('does not release a broken bike', () => {
+    //   const dockingStation = new DockingStation();
+      
+    // });
   });
 
-  it('does not release a bike if no bike is available', () => {
-    const dockingStation = new DockingStation();
-    expect(function () { dockingStation.releaseBike() }).toThrowError('No Bikes Available')
-  });
-
-  it('has a bike docked', () => {
-    const dockingStation = new DockingStation();
-    dockingStation.dockBike() 
-    expect(dockingStation.bikes).toEqual(['Bike'])
-  });
-
-  it('can dock a bike', () => {
-    const dockingStation = new DockingStation();
-    expect(dockingStation.dockBike()).toEqual('Your Bike has been docked')
-  });
-
-  it('does not allow docking when the docking station is full', () => {
-    const dockingStation = new DockingStation();
-    for (var i = 0; i < 20; i++) {
+  describe('docking bike', () => {
+    it('has a bike docked', () => {
+      const dockingStation = new DockingStation();
       dockingStation.dockBike() 
-    };
-    expect(function () { dockingStation.dockBike() }).toThrowError('Docking Station is Full. No space to dock Bike')
+      expect(dockingStation.bikes).toEqual(['Bike'])
+    });
+  
+    it('can dock a bike', () => {
+      const dockingStation = new DockingStation();
+      expect(dockingStation.dockBike()).toEqual('Your Bike has been docked')
+    });
+  
+    it('does not allow docking when the docking station is full', () => {
+      const dockingStation = new DockingStation();
+      for (var i = 0; i < 20; i++) {
+        dockingStation.dockBike() 
+      };
+      expect(function () { dockingStation.dockBike() }).toThrowError('Docking Station is Full. No space to dock Bike')
+    });
   });
 }); 
