62393b4b78eb322addbd694af7b6190e268153e6
hansa-lallu <hansalallu@gmail.com>
2020-06-23 06:08:31 +0100

    refactored tests

--- spec/dockingStationSpec.js
+++ spec/dockingStationSpec.js
@@ -1,39 +1,40 @@
 const { DockingStation } = require('../src/dockingStation')
 
-let dockingStation
 
 describe('DockingStation', () => {
 
-  beforeAll(() => {
-    dockingStation = new DockingStation();
-  });
-
   it('can release a bike from a docking station', () => {
+    const dockingStation = new DockingStation();
+    dockingStation.dockBike()
     expect(dockingStation.releaseBike()).toEqual('Here is your Bike');
   });
 
   it('can dock a bike', () => {
+    const dockingStation = new DockingStation();
     expect(dockingStation.dockBike()).toEqual('Your Bike has been docked')
   });
 
-  it('has a bike docked', () => {
-    dockingStation = new DockingStation('Bike');
-    expect(dockingStation.bikes).toEqual(['Bike'])
-  });
-
   it('does not release a bike if no bike is available', () => {
-    dockingStation.releaseBike();
-    expect(dockingStation.releaseBike()).toEqual('No Bikes Available')
+    const dockingStation = new DockingStation();
+    expect(function () { dockingStation.releaseBike() }).toThrowError('No Bikes Available')
   });
 
   it('has a default capacity', () => {
+    const dockingStation = new DockingStation();
     expect(dockingStation.capacity).toEqual(20)
   });
 
   it('does not allow docking when the docking station is full', () => {
+    const dockingStation = new DockingStation();
     for (var i = 0; i < 20; i++) {
       dockingStation.dockBike() 
     };
     expect(function () { dockingStation.dockBike() }).toThrowError('No space to dock Bike')
+  });
+
+  it('has a bike docked', () => {
+    const dockingStation = new DockingStation();
+    dockingStation.dockBike() 
+    expect(dockingStation.bikes).toEqual(['Bike'])
   });
 }); 
--- src/dockingStation.js
+++ src/dockingStation.js
@@ -1,12 +1,12 @@
 class DockingStation {
-  constructor(name) {
-    this.bikes = [name]
+  constructor() {
+    this.bikes = []
     this.capacity = 20 
   };
   
   releaseBike() {
     if (this.bikes.length === 0) { 
-      return 'No Bikes Available' 
+      throw new Error('No Bikes Available')
     } else {
     this.bikes.pop()
       return 'Here is your Bike'
