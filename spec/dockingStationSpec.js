a6d1e2d8ee87cbc807286f2c10d713e9f0a2f6f6
hansa-lallu <hansalallu@gmail.com>
2020-06-23 04:06:43 +0100

    first test creating docking station class

---
+++ spec/dockingStationSpec.js
@@ -0,0 +1,8 @@
+const { DockingStation } = require('../src/dockingStation')
+
+describe('DockingStation', () => {
+  it('can release a bike from a docking station', () => {
+    const dockingStation = new DockingStation();
+    expect(dockingStation.releaseBike().toEqual(Bike))
+  });
+}); 
